0df52d97967652c74cbb06a2f1e169b94fac1dff
Athlete Decoded <k@athletedecoded.com>
2021-08-10 08:41:18 +1000

    Create S3 Bucket & instance using env variables

---
+++ s3.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const S3 = require('aws-sdk/clients/s3');
+
+const bucketName = preocess.env.AWS_BUCKET_NAME;
+const region = process.env.AWS_BUCKET_REGION;
+const accessKeyId = process.env.AWS_ACCESS_KEY;
+const secretAccessKey = process.env.AWS_SECRET_KEY;
+
+const S3 = new S3 ({
+    region,
+    accessKeyId,
+    secretAccessKey
+})
+
+// Upload file to S3
+export function uploadFile(file) {
+    
+}
+
+// Downloads from S3
