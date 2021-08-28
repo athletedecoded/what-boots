b9ece74019b7bd285c72b54183e45285835c8eec
Kahlia <kahlia.hogg@gmail.com>
2021-08-28 13:57:22 +1000

    Configure TF.js model
    Edit model to remove hub.Keras layer (conflict with tf.js conversion). Process image input to 224x224x3 and tensor dim. Successfully returning prediction values :)

--- server.js
+++ server.js
@@ -21,16 +21,31 @@ app.use(bodyParser.json());
 
 const tf = require('@tensorflow/tfjs');
 const tfnode = require('@tensorflow/tfjs-node');
+const fs = require('fs');
+const mobilenet = require('@tensorflow-models/mobilenet');
+
+let path = "./public/uploads/test.jpg"
+
+const loadImage = path => {
+  const imageBuffer = fs.readFileSync(path);
+  let image = tfnode.node.decodeImage(imageBuffer,3);
+  image = image.cast('float32').div(255); //Normalise
+  image = tf.image.resizeBilinear(image, size = [224, 224]); // can also use tf.image.resizeNearestNeighbor
+  image = image.expandDims(); // Add Tensor dimension
+  // console.log(image.shape)
+  return image;
+}
 
 const imageClassification = async (path) => {
-    console.log("Hit clf")
-    const image = tfnode.node.decodeImage(path);
-    const model = await tf.loadLayersModel('https://drive.google.com/drive/folders/1MpQzeFNB-QyPyszMczDEOPOq2cyQAkZg?usp=sharing');
-    const prediction = model.predict(image);
+    const image = loadImage(path);
+    console.log("Image processed", image)
+    const model = await tf.loadLayersModel('https://raw.githubusercontent.com/athletedecoded/what-boots/master/tfjs/model.json');
+    console.log("Model loaded", model.summary())
+    const prediction = await model.predict(image).data();
+
     console.log('Classification Results:', prediction);
 }
 
-let path = "https://assets.adidas.com/images/w_600,f_auto,q_auto/179d567e197c4de7bf3dab0300b415ad_9366/Predator_Mutator_20+_Firm_Ground_Boots_Black_EF1565_01_standard.jpg"
 imageClassification(path);
 
 // app.get('/images/:key', (req,res) => {
